ce9e13906dfbc058d634c621d37e2359bb5e25a0
Walid-N-bit <wnouicer24@student.oulu.fi>
2026-03-11 13:38:14 +0200

    _

--- app.py
+++ app.py
@@ -297,9 +297,10 @@ def clone_to_container(name: str):
 
 def install_dependencies(name: str):
     inputs = f"""sudo lxc exec {name} -- bash -c '
-    sudo apt update &&
+    sudo apt update -y &&
+    sudo apt upgrade -y &&
     sudo apt install -y git python3-pip &&
-    sudo apt install python3-venv &&
+    sudo apt install python3-venv -y &&
     cd fl_app &&
     python3 -m venv venv &&
     source venv/bin/activate &&
